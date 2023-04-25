7f0fd744d59fe1e5e0aeda894b65b20e3c23d342
ecto <cam@campedersen.com>
2023-04-25 08:08:56 -0400

    meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta property="og:title" content="emerald" />
+        <meta name="image" content="https://emerald.io/preview.jpg" />
+        <meta name="og:image" content="https://emerald.io/preview.jpg" />
         <meta name="description" content="internet toy factory" />
+        <meta name="og:description" content="internet toy factory" />
         <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400&display=swap"
